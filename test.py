6883267ff539fddc98a9f6d478ab6b3c7b7e2948
Junior Lima <junior@Estoque>
2018-10-18 15:33:58 -0300

    Alteração 2 test.py

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "Projeto DevOps Essential.")
+        self.assertRegex(result.data.decode(), "Projeto DevOps Essentials.")
 
 
 if __name__ == "__main__":
